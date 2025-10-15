e923d88026c268692c4373a4873be6aa7cf97091
ELFA <140125432+karimtz999@users.noreply.github.com>
2025-10-15 21:45:28 +0100

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,7 +0,0 @@
-/** postcss.config.mjs */
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    // you *do not* need to explicitly include autoprefixer; Tailwind v4 handles that internally
-  },
-};
